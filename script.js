3a735b7ef987990789c1fa4c63fbef1ba3cd989f
Martin Terskin <martinas.terskin@gmail.com>
2016-04-18 15:19:27 -0400

    finn #22

--- script.js
+++ script.js
@@ -179,10 +179,10 @@ function arithmeticGamePopulate () {
                           function fadeOutEffect() {
                               var fadeTarget = document.getElementsByClassName('Finn')[0];
                               var fadeEffect = setInterval(function () {
-                                  if (fadeTarget.style.opacity > 0.9) {
+                                  if (fadeTarget.style.opacity > 0.99) {
                                       clearInterval(fadeEffect);
                                   } else {
-                                      fadeTarget.style.opacity += 0.1;
+                                      fadeTarget.style.opacity += 0.01;
                                   }
                               }, 1000);
                           }
