31bfb338ce104f1026550dd665b539d1db8a1c02
Martin Terskin <martinas.terskin@gmail.com>
2016-04-18 15:38:12 -0400

    finn #24

--- script.js
+++ script.js
@@ -184,7 +184,7 @@ function arithmeticGamePopulate () {
                                   } else {
                                       fadeTarget.style.opacity += 0.1;
                                   }
-                              }, 100);
+                              }, 50);
                           }
                               fadeOutEffect();
                         }
